eb1e1f964a4f10621da1551323060ecd9fe6a6f6
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-01-08 20:08:00 +0000

    Updated command error message reporting

--- src/main/kotlin/me/bardy/bot/listeners/MessageListener.kt
+++ src/main/kotlin/me/bardy/bot/listeners/MessageListener.kt
@@ -6,6 +6,7 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException
 import me.bardy.bot.command.CommandContext
 import me.bardy.bot.config.bot.BotConfig
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import org.apache.logging.log4j.LogManager
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 
@@ -14,21 +15,27 @@ class MessageListener(private val botConfig: BotConfig, private val dispatcher: 
 
     override fun onMessageReceived(event: MessageReceivedEvent) {
         if (!event.message.contentRaw.startsWith(botConfig.prefix)) return
-        val message = event.message.contentRaw.removePrefix(botConfig.prefix)
+        val command = event.message.contentRaw.removePrefix(botConfig.prefix)
+        val context = CommandContext(event.guild, event.channel, event.member, event.message)
 
         try {
-            dispatcher.execute(parseCommand(CommandContext(event.guild, event.channel, event.member, event.message), message))
+            val parseResults = parseCommand(context, command)
+            dispatcher.execute(parseResults)
         } catch (exception: CommandSyntaxException) {
-            val exceptionMessage = when (exception.type) {
-                CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherParseException() -> "a parse exception"
-                CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherUnknownArgument() -> "an unknown argument exception"
-                CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherUnknownCommand() -> "an unknown command exception"
-                else -> "Unknown error when attempting to execute command $message"
-            }
-            event.channel.sendMessage("I seem to have encountered $exceptionMessage when trying to execute that.").queue()
+            val commandName = command.split(" ").first()
+            val exceptionMessage = exception.rawMessage.string
+            val message = "I seem to have encountered an error when trying to execute command '$commandName': $exceptionMessage"
+            event.channel.sendMessage(message).queue()
+        } catch (exception: Throwable) {
+            LOGGER.error("Unexpected error trying to execute command '$command'!", exception)
         }
     }
 
     @Cacheable("parse_results")
     fun parseCommand(context: CommandContext, command: String): ParseResults<CommandContext> = dispatcher.parse(command, context)
+
+    companion object {
+
+        private val LOGGER = LogManager.getLogger()
+    }
 }
