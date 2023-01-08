46ddbefec20b6505e832ee9abcb8773d4f392915
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-01-08 20:10:57 +0000

    Removed API and metrics

--- src/main/kotlin/me/bardy/bot/config/bot/ServicesConfig.kt
+++
@@ -1,8 +0,0 @@
-package me.bardy.bot.config.bot
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
-
-@ConfigurationProperties("services")
-@ConstructorBinding
-data class ServicesConfig(val metrics: Boolean, val api: Boolean)
